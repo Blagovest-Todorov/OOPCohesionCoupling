e687520f4f6aa9f9e3917cd9bce1f2fefa84c0d0
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-12 17:37:52 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6.OOPCohesionCoupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cohesion --is connected to relaton to the classes, hierarchy of classes // Kind of independancy our classes to work !
+            // Cohesion keep the classes self-containt, kind a independant each other 
+            // How stringly the classes are related each other // my how good they inetract with the outside world ecxept with certain interface 
+            // Harddisk is example of strong Cohesion; CD Rom ;
+            // keep yopur classes neat -to serve onne purpose, one aim !
+            // Math class  that has methods , is an example of strong Cohesion --do some maths operations!
+            //Math class does not care aout what is opuside, just feed it with data and you get result you expect //
+            //Spaghetti is example of weak cohesion. // Independancy of working of classes !
+            // Keep You classes encapsulateed, expose only what is needed, not more 
+            // 
+            // It is good to keep the Cohesion high stong, level and the coupling low lewel !
+
+            // Coupling is the opposite --how much dependant our classes are one each other to work . 
+            //the things are strong connected, very dependant each other , strong dependant each other
+            // Questions are quaranteed in life, answers not !
+
+
+        }
+    }
+}
